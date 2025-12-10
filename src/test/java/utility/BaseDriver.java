fe1327e1cac8c3c85da740c28ef9578d9c11b668
MrAtas35 <gurhanatas45@gmail.com>
2025-12-10 21:20:36 +0300

    Jenkinsfile updated8

--- src/test/java/utility/BaseDriver.java
+++ src/test/java/utility/BaseDriver.java
@@ -1,4 +1,50 @@
-package utility;
+
+        package utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class BaseDriver {
+
+    public static WebDriver driver(String url) {
+        ChromeOptions chromeOptions=new ChromeOptions();
+        chromeOptions.addArguments("--headless=new"); // chrome sayfasını açmadan arka planda testi çalıştırıyor.
+        chromeOptions.addArguments("--windows-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        /** Bu bölüm ilk kullandigimiz sitede cikan cookies icindi, ortak her siteye uymadigi
+         icin yoruma aldik */
+        try {
+            WebElement element =
+                    driver.findElement(By.xpath("//div[@class='fc-footer-buttons']/button[1]"));
+
+            element.click();
+            throw new NoSuchElementException("Element sayfada yok");
+
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Bilinmiyen bir hata olustu");
+        }
+
+        return driver;
+    }
+
+    public static void threadWait(int second) {
+        try {
+            Thread.sleep(second*1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
